0a24593da0162f54ea39ad3ba36069b056aad482
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-07 19:52:31 +0530

    Add: Function to append node to list

--- main/java/linked_list/LinkedListClass.java
+++ main/java/linked_list/LinkedListClass.java
@@ -1,21 +1,21 @@
 package linked_list;
 
 public class LinkedListClass<Key> {
-	
+
 	public LinkedListFunction<Key> headNode;
 	public LinkedListFunction<Key> tailNode;
-	
+
 	public LinkedListClass() {
 		this.headNode = null;
 		this.tailNode = null;
 	}
-	
-	public void append(LinkedListFunction<Key> newNode) {
-		
+
+	public void add(LinkedListFunction<Key> newNode) {
+
 		if(this.tailNode==null) {
 			this.tailNode=newNode;
 		}
-		
+
 		if(this.headNode==null) {
 			this.headNode=newNode;
 		}else {
@@ -23,6 +23,21 @@ public class LinkedListClass<Key> {
 			LinkedListFunction<Key> tempNode=this.headNode;
 			this.headNode=newNode;
 			(this.headNode).setNextNode(tempNode);
+		}
+	}
+
+	public void append(LinkedListFunction<Key> newNode) {
+
+		if(this.headNode==null) {
+			this.headNode=newNode;
+		}
+
+		if(this.tailNode==null) {
+			this.tailNode=newNode;
+		}else {
+			//append entry here
+			(this.tailNode).setNextNode(newNode);
+			this.tailNode=newNode;
 		}
 	}
 }
