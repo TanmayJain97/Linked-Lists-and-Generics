a251b3cbc82cfb146044b749ef1c75fe8c4da8eb
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-07 23:44:32 +0530

    Refactor: Changed Test Cases to ensure 40 is being removed and size returned as 3

--- test/java/linked_list/LinkedListTest.java
+++ test/java/linked_list/LinkedListTest.java
@@ -10,40 +10,48 @@ public class LinkedListTest {
 	static private LinkedListNode<Integer> Node1;
 	static private LinkedListNode<Integer> Node2;
 	static private LinkedListNode<Integer> Node3;
+	static private LinkedListNode<Integer> Node4;
 	
 	@BeforeClass
 	public static void init() {
 		//Passing key value
 		Node1 = new LinkedListNode<Integer>(56);
 		Node2 = new LinkedListNode<Integer>(30);
-		Node3 = new LinkedListNode<Integer>(70);
+		Node3 = new LinkedListNode<Integer>(40);
+		Node4 = new LinkedListNode<Integer>(70);
 		LinkedList = new LinkedListClass<Integer>();
 		
 		//Appending
 		LinkedList.add(Node1);
 		LinkedList.append(Node2);
 		LinkedList.append(Node3);
+		LinkedList.append(Node4);
 		
 		//Display Original List
 		System.out.print("Original List=");
 		LinkedList.showList();
 		
-		//Inserting 40 after 30
-		LinkedList.insertAfter(30, 40);
+		//Deleting node 40
+		LinkedList.removeNode(40);
 		
 		//Display New List
 		System.out.print("Original List=");
 		LinkedList.showList();
 	}
 	
-	//Checking for position of 40
+	//Checking for removal of 40
 	@Test
-	public void When40SearchedItShouldLieBefore70() {
-		assertEquals(Node3,LinkedList.search(40).getNextNode());
+	public void When40SearchedItShouldReturnNotFoundMessage() {
+		assertEquals(LinkedList.tailNode,LinkedList.search(40));
 	}
 	
 	@Test
-	public void When40SearchedItShouldLieAfter30() {
-		assertEquals(40,(int)Node2.getNextNode().getKey());
+	public void NodeWith70ShouldComeAfter30() {
+		assertEquals(Node4,Node2.getNextNode());
+	}
+	
+	@Test
+	public void SizeOfListShouldReturn3() {
+		assertEquals(3,LinkedList.removeNode(40));
 	}
 }
