12989ceb8cb003fa95deab2c961ab673165e69a2
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-08 00:57:13 +0530

    Refactor: Changed test case to ensure head and tail are ordered

--- test/java/linked_list/LinkedListTest.java
+++ test/java/linked_list/LinkedListTest.java
@@ -32,26 +32,21 @@ public class LinkedListTest {
 		LinkedList.showList();
 		
 		//Deleting node 40
-		LinkedList.removeNode(40);
+		LinkedList.makeOrderedList();
 		
 		//Display New List
 		System.out.print("Original List=");
 		LinkedList.showList();
 	}
 	
-	//Checking for removal of 40
+	//Checking for ordering
 	@Test
-	public void When40SearchedItShouldReturnNotFoundMessage() {
-		assertEquals(LinkedList.tailNode,LinkedList.search(40));
+	public void HeadNodeShouldBe30() {
+		assertEquals(Node2,LinkedList.headNode);
 	}
 	
 	@Test
-	public void NodeWith70ShouldComeAfter30() {
-		assertEquals(Node4,Node2.getNextNode());
-	}
-	
-	@Test
-	public void SizeOfListShouldReturn3() {
-		assertEquals(3,LinkedList.removeNode(40));
+	public void TailNodeShouldBe70() {
+		assertEquals(Node4,LinkedList.tailNode);
 	}
 }
