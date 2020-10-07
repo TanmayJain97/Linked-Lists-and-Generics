c0f8e6748475b68b3608e16ae5e5e96068a73cd6
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-07 19:21:51 +0530

    Add: Added test to check correct formation of Linked List

---
+++ test/java/linked_list/LinkedListTest.java
@@ -0,0 +1,43 @@
+package linked_list;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	LinkedListClass<Integer> LinkedList;
+	private LinkedListNode<Integer> Node1;
+	private LinkedListNode<Integer> Node2;
+	private LinkedListNode<Integer> Node3;
+	
+	@Before
+	public void init() {
+		//Passing key value
+		Node1 = new LinkedListNode<Integer>(56);
+		Node2 = new LinkedListNode<Integer>(30);
+		Node3 = new LinkedListNode<Integer>(70);
+		LinkedList = new LinkedListClass<Integer>();
+		
+		//Appending
+		LinkedList.append(Node3);
+		LinkedList.append(Node2);
+		LinkedList.append(Node1);
+	}
+	
+	//Checking for append of all 3 values
+	@Test
+	public void WhenNode1appendedShouldReturnTrue() {
+		assertTrue((LinkedList.headNode).equals(Node1));
+	}
+
+	@Test
+	public void WhenNode2appendedShouldReturnTrue() {
+		assertTrue(((LinkedList.headNode).getNextNode()).equals(Node2));
+	}
+	
+	@Test
+	public void WhenNode3appendedShouldReturnTrue() {
+		assertTrue((LinkedList.tailNode).equals(Node3));
+	}
+}
