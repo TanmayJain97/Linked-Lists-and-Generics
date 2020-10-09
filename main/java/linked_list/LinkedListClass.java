ea1685e2013f44cae1e5b991f10c9bc1dede69aa
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-10 00:54:49 +0530

    Refactor: makeOrderedList function corrected

--- main/java/linked_list/LinkedListClass.java
+++ main/java/linked_list/LinkedListClass.java
@@ -1,11 +1,10 @@
 package linked_list;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class LinkedListClass<Key> {
+public class LinkedListClass<Key>{
 
 	public LinkedListFunction<Key> headNode;
 	public LinkedListFunction<Key> tailNode;
@@ -45,7 +44,7 @@ public class LinkedListClass<Key> {
 			this.tailNode=newNode;
 		}
 	}
-	
+
 	public void showList(){
 		System.out.print("Linked List = ");
 		LinkedListFunction<Key> tempNode=headNode;
@@ -56,19 +55,19 @@ public class LinkedListClass<Key> {
 		}while(tempNode.getNextNode() != null);
 		System.out.println(tempNode.getKey());
 	}
-	
+
 	public void insert(LinkedListFunction<Key> beforeNode, LinkedListFunction<Key> newNode) {
 		LinkedListFunction<Key> tempNode=beforeNode.getNextNode();
 		beforeNode.setNextNode(newNode);
 		newNode.setNextNode(tempNode);
 	}
-	
+
 	public void pop() {
 		LinkedListFunction<Key> tempNode=headNode;
 		headNode=headNode.getNextNode();
 		System.out.println("Removed "+tempNode.getKey()+" from Linked List.");
 	}
-	
+
 	public void popLast() {
 		LinkedListFunction<Key> tempNode=headNode;
 		while((!(tempNode.getNextNode()).equals(tailNode))) {
@@ -78,7 +77,7 @@ public class LinkedListClass<Key> {
 		tailNode.setNextNode(null);
 		System.out.println("Removed "+tempNode.getKey()+" from Linked List.");
 	}
-	
+
 	public LinkedListNode<Key> search(Key data) {
 		LinkedListFunction<Key> tempNode=headNode;
 		while(tempNode!=null && tempNode.getNextNode()!=null) {
@@ -87,19 +86,19 @@ public class LinkedListClass<Key> {
 			}
 			tempNode=tempNode.getNextNode();
 		}
-		
+
 		System.out.println("No such value exists in LinkedList.");
 		return (LinkedListNode<Key>) tailNode;
 	}
-	
+
 	public void insertAfter(Key before_data,Key data) {
 		LinkedListNode<Key> before_Node=search(before_data);
 		LinkedListNode<Key> newNode=new LinkedListNode<Key>(data);
-		
+
 		//inserting
 		insert(before_Node, newNode);
 	}
-	
+
 	public int removeNode(Key data) {
 		LinkedListFunction<Key> tempNode=headNode;
 		int count=1;
@@ -113,27 +112,20 @@ public class LinkedListClass<Key> {
 		}
 		return count;
 	}
+
+	public void makeOrderedList(LinkedListNode<Key> newNode) {
 	
-	public void makeOrderedList() {
-		LinkedListFunction<Integer> tempNode=(LinkedListFunction<Integer>) headNode;
-		Stream.Builder<Integer> key_stream=Stream.builder();
-		while(tempNode!=null && tempNode.getNextNode()!=null) {
-			key_stream.accept(tempNode.getKey());
-		}
-		
-		//Sorting using streams and passing to arraylist
-		ArrayList<Integer> nodes= (ArrayList<Integer>) ((Stream) key_stream)
-				.sorted((key1,key2)->
-						((Integer) key1).compareTo((Integer) key2))
-				.collect(Collectors.toList());
-		
-		LinkedListNode<Integer> head = new LinkedListNode<Integer>(nodes.get(0));
-		nodes.remove(0);
-		headNode=(LinkedListFunction<Key>) head;
-		add(headNode);
-		for(int keys:nodes) {
-			LinkedListNode<Integer> node = new LinkedListNode<Integer>(keys);
-			append((LinkedListFunction<Key>) node);
+		LinkedListFunction<Key> tempNode;
+		if (headNode == null || ((Integer) headNode.getKey())>=((Integer) newNode.getKey())) {
+			newNode.setNextNode(headNode);
+			headNode = newNode;
+		} else {
+			tempNode = headNode;
+			while (tempNode.getNextNode() != null && ((Integer) newNode.getKey())>((Integer) tempNode.getKey())) {
+				tempNode = tempNode.getNextNode();
+				newNode.setNextNode(tempNode.getNextNode());
+				tempNode.setNextNode(newNode);
+			}
 		}
 	}
 }
